0b8135a1bbad222a83aaf901c7f4d1b7f87e2259
mfarhanz <paan.zainudin97@gmail.com>
2023-07-29 19:02:30 +0800

    cleanup init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 --[[
+:q
 
 Kickstart Guide:
 
@@ -230,6 +231,17 @@ vim.o.completeopt = 'menuone,noselect'
 vim.o.termguicolors = true
 
 -- [[ Basic Keymaps ]]
+
+-- window management
+vim.keymap.set("n", "<leader>sv", "<C-w>v")        -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s")        -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=")        -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", ":close<CR>")    -- close current split window
+
+vim.keymap.set("n", "<leader>to", ":tabnew<CR>")   -- open new tab
+vim.keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+vim.keymap.set("n", "<leader>tn", ":tabn<CR>")     --  go to next tab
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>")     --  go to previous tab
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
